afb15f73fa2459295438e14df0c9047782a867d3
John Kozak <jk@thameslighter.net>
2014-07-20 18:05:39 +0100

    example code for `charjes`

--- charjes.js
+++ charjes.js
@@ -1,2 +1,14 @@
 // parser for javascript extended with CHR
 
+// example:
+//
+//  store facts() {
+//      rule (['update-instrument',name,new_name];-['instrument',name]) {
+//  	    +['instrument',new_name];
+//      }
+//  }
+//  facts.add(['instrument','5T18']);
+
+"use strict";
+
+
