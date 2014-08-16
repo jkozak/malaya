ebdee86b86f68bc332e1f2ff009638deaa4b0bde
John Kozak <jk@thameslighter.net>
2014-08-16 20:35:18 +0100

    start of CHRjs

--- chrjs.el
+++ chrjs.el
@@ -1,2 +1,15 @@
 ;;; emacs mode for charjes (javascript+CHR)
 
+(require 'js)
+
+(define-derived-mode chrjs-mode js-mode "CHRjs"
+    "chrjs mode is a major mode for editing chrjs files"
+  
+    ;; you again used quote when you had '((mydsl-hilite))
+    ;; I just updated the variable to have the proper nesting (as noted above)
+    ;; and use the value directly here
+    ;(setq font-lock-defaults mydsl-font-lock-defaults)
+  
+    )
+  
+(provide 'chrjs-mode)
---
+++ test/chrjs.js
@@ -0,0 +1,4 @@
+require("sweet.js");
+var chr    = require("../chrjs.sjs");
+
+var assert = require("assert");
