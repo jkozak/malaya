c372c8f3b2305935871c39eb44d132f8fee64b74
John Kozak <jk@thameslighter.net>
2014-08-23 09:59:54 +0100

    aggregates work a bit

--- test/chr.js
+++ test/chr.js
@@ -1,6 +1,7 @@
 var chr    = require("../chr.js");
 
 var assert = require("assert");
+var   util = require('../util.js');
 
 describe('match()',function() {
     var context;
@@ -95,5 +96,93 @@ describe('match()',function() {
 	context = {};
 	assert.ok(match({'':ps},{r:'a',s:1},context));
 	assert.deepEqual(context,{ps:{r:'a',s:1}});
+    });
+});
+
+describe('Store',function() {
+    it("should add and remove facts",function() {
+	var store = new chr.Store();
+	assert.equal(store.size(),0);
+	var t1 = store.add([1,2,3]);
+	assert.equal(store.size(),1);
+	var t2 = store.add([1,2,3]);
+	assert.ok(t2>t1);
+	assert.equal(store.size(),2);	    // multiset
+	var t3 = store.add({a:1,b:2});
+	assert.ok(t3>t2);
+	assert.equal(store.size(),3);
+	var t4 = store.add({a:1,b:2});
+	assert.ok(t4>t3);
+	assert.equal(store.size(),4);	    // multiset
+	store.remove(t1);
+	assert.equal(store.size(),3);
+	store.remove(t2);
+	assert.equal(store.size(),2);
+	store.remove(t4);
+	assert.equal(store.size(),1);
+	store.remove(t3);
+	assert.equal(store.size(),0);
+    });
+    describe('Aggregate',function() {
+	it("should zero gracefully",function() {
+	    var store = new chr.Store();
+	    var  aggr = new chr.Aggregate([["X",new chr.Variable('x')]],
+					  function(_){return true;},
+					  1337,
+					  function(v,_,ctx){return v+ctx['x'];} );
+	    assert.equal(store.aggregate(aggr),1337);
+	    store.add(["Y",100]);
+	    assert.equal(store.aggregate(aggr),1337);
+	});
+	it("should guard firmly",function() {
+	    var store = new chr.Store();
+	    var  aggr = new chr.Aggregate([["X",new chr.Variable('x')]],
+					  function(_){return false;},
+					  0,
+					  function(v,_,ctx){return v+ctx['x'];} );
+	    store.add(["X",100]);
+	    assert.equal(store.aggregate(aggr),0);
+	});
+	it("should match a single head",function() {
+	    var store = new chr.Store();
+	    var  aggr = new chr.Aggregate([["X",new chr.Variable('x')]],
+					  function(_){return true;},
+					  0,
+					  function(v,_,ctx){return v+ctx['x'];} );
+	    assert.equal(store.aggregate(aggr),0);
+	    store.add(["X",100]);
+	    assert.equal(store.aggregate(aggr),100);
+	    store.add(["X",1]);
+	    assert.equal(store.aggregate(aggr),101);
+	    store.add(["Y",99]);
+	    assert.equal(store.aggregate(aggr),101);
+	});
+	it("should match dual heads",function() {
+	    var store = new chr.Store();
+	    var  aggr = new chr.Aggregate([["X",new chr.Variable('x'),new chr.Variable('p')],
+					   ["X",new chr.Variable('x'),new chr.Variable('q')] ],
+					  function(ctx){return ctx['p']>ctx['q'];},
+					  0,
+					  function(v,_,ctx){return v+ctx['p']+ctx['q'];} );
+	    store.add(["X",1,10]);
+	    store.add(["X",2,20]);
+	    store.add(["X",2,30]);
+	    assert.equal(store.aggregate(aggr),50);
+	});
+	it("should match triple heads",function() {
+	    var store = new chr.Store();
+	    var  aggr = new chr.Aggregate([["X",new chr.Variable('x'),new chr.Variable('p')],
+					   ["X",new chr.Variable('x'),new chr.Variable('q')],
+					   ["X",new chr.Variable('x'),new chr.Variable('r')]],
+					  function(ctx){return ctx['p']>ctx['q'] && ctx['q']>ctx['r'];},
+					  0,
+					  function(v,_,ctx){return v+ctx['p']+ctx['q']+ctx['r'];} );
+	    store.add(["X",1,10]);
+	    store.add(["X",2,20]);
+	    store.add(["X",2,30]);
+	    assert.equal(store.aggregate(aggr),0);
+	    store.add(["X",2,40]);
+	    assert.equal(store.aggregate(aggr),90);
+	});
     });
 });
