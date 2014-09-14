3bb948839b62b149900d3f8b4bb99d1566950da9
John Kozak <jk@thameslighter.net>
2014-09-14 20:50:11 +0100

    start async prevalence; benchmark saving and loading

--- benchmark/prevalence.js
+++ benchmark/prevalence.js
@@ -19,73 +19,7 @@ function clonefile(filename) {
     return fn;
 }
 
-if (false) {
-    // these benchmarks are to see where the time goes in journalising
-    var buf250 = "";
-    for (var i=0;i<25;i++)
-	buf250 += "0123456789";
-
-    suite("bits",function() {
-	var x;
-	bench("new Date()",function() {
-	    x = new Date();
-	});
-
-	bench("util.serialise+\\n",function() {
-	    x = util.serialise(["x",new Date()])+"\n";
-	});
-    });
-
-    suite("overwrite",function() {
-	var fd1,fd2,fd3;
-	before(function() {
-	    var dir = temp.mkdirSync();
-	    fd1 = fs.openSync(path.join(dir,"xxx1"),consts.O_CREAT|consts.O_WRONLY);
-	    fd2 = fs.openSync(path.join(dir,"xxx2"),consts.O_CREAT|consts.O_WRONLY|consts.O_SYNC);
-	    fd3 = fs.openSync(path.join(dir,"xxx3"),consts.O_CREAT|consts.O_WRONLY|consts.O_DSYNC);
-	});
-	bench("no sync",function() {
-	    fs.writeSync(fd1,buf250,buf250.length,0);
-	});
-	bench("O_SYNC",function() {
-	    fs.writeSync(fd2,buf250,buf250.length,0);
-	});
-	bench("O_DSYNC",function() {
-	    fs.writeSync(fd3,buf250,buf250.length,0);
-	});
-	after(function() {
-	    fs.closeSync(fd1);
-	    fs.closeSync(fd2);
-	    fs.closeSync(fd3);
-	});
-    });
-
-    suite("append",function() {
-	var fd1,fd2,fd3;
-	before(function() {
-	    var dir = temp.mkdirSync();
-	    fd1 = fs.openSync(path.join(dir,"xxx1"),consts.O_APPEND|consts.O_CREAT|consts.O_WRONLY);
-	    fd2 = fs.openSync(path.join(dir,"xxx2"),consts.O_APPEND|consts.O_CREAT|consts.O_WRONLY|consts.O_SYNC);
-	    fd3 = fs.openSync(path.join(dir,"xxx3"),consts.O_APPEND|consts.O_CREAT|consts.O_WRONLY|consts.O_DSYNC);
-	});
-	bench("no sync",function() {
-	    fs.writeSync(fd1,buf250,buf250.length,null);
-	});
-	bench("O_SYNC",function() {
-	    fs.writeSync(fd2,buf250,buf250.length,null);
-	});
-	bench("O_DSYNC",function() {
-	    fs.writeSync(fd3,buf250,buf250.length,null);
-	});
-	after(function() {
-	    fs.closeSync(fd1);
-	    fs.closeSync(fd2);
-	    fs.closeSync(fd3);
-	});
-    });
-}
-
-// ['fsync','o_sync','o_dsync','none','kludge'] are the full set of options here
+// ['fsync','fdatasync','o_sync','o_dsync','none','kludge'] is the full set of options here
 ['none','kludge'].forEach(function(f) {
     suite("journalise, sync style: "+f,function() {
 	var wbl;
@@ -101,5 +35,29 @@ if (false) {
 	after(function() {
 	    wbl.close();
 	});
+    });
+});
+
+suite("load/save/load 10k small JSON items",function() {
+    var wbl;
+    before(function() {
+	var dir = temp.mkdirSync();
+	wbl = prvl.wrap(dir,clonefile(bl_src),{audit:false,sync_journal:'none'});
+	wbl.init();
+	wbl.open();
+	for (var i=0;i<1000;i++)
+	    wbl.update(['item',[[[i],{a:1,b:2,c:3,d:4,e:5}],{weeble:99}]]);
+    });
+    bench("load from journal",function() {
+	wbl.load();
+    });
+    bench("save image",function() {
+	wbl.save();
+    });
+    bench("load from image",function() {
+	wbl.load();
+    });
+    after(function() {
+	wbl.close();
     });
 });
