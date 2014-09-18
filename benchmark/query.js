b081b1be9eaf64a425dd003add1c6980e71e936e
John Kozak <jk@thameslighter.net>
2014-09-18 18:39:30 +0100

    queries now called Snap; internal structure more like rules-to-be
    use Context object
    use immutable sets a bit (reverse previous slowdown)
    more tests

--- benchmark/query.js
+++ benchmark/query.js
@@ -2,6 +2,12 @@
 
 var chr = require('../chr.js');
 
+var Match  = chr._private.ItemMatch; // +++ replace these with chrjs notation +++
+var Guard  = chr._private.ItemGuard;
+var Delete = chr._private.ItemDelete;
+var Add    = chr._private.ItemAdd;
+var Fail   = chr._private.ItemFail;
+
 suite('query',function() {
     var   total = 0;
     var samples = [10,50,100];
@@ -12,9 +18,9 @@ suite('query',function() {
 	    store.add(["X",i,20]);
 	}
 	bench("1 headed, store"+n,function() {
-	    total=0;
-	    store.select(new chr.Select([["X",new chr.Variable('x'),new chr.Variable('p')]],
-					function(ctx){total+=ctx['p'];} ));
+	    store.snap(new chr.Snap([new Match(["X",new chr.Variable('x'),new chr.Variable('p')])],
+				    0,
+				    function(x,ctx){return x+ctx['p'];} ));
 	});
     });
     samples.forEach(function(n) {
@@ -24,11 +30,11 @@ suite('query',function() {
 	    store.add(["X",i,20]);
 	}
 	bench("2 headed, store"+n,function() {
-	    total=0;
-	    store.select(new chr.Select([["X",new chr.Variable('x'),new chr.Variable('p')],
-					 ["X",new chr.Variable('x'),new chr.Variable('q')] ],
-					function(ctx){return ctx['p']>ctx['q'];},
-					function(ctx){total+=ctx['p']+ctx['q'];} ));
+	    store.snap(new chr.Snap([new Match(["X",new chr.Variable('x'),new chr.Variable('p')]),
+				     new Match(["X",new chr.Variable('x'),new chr.Variable('q')]),
+				     new Guard(function(ctx){return ctx['p']>ctx['q'];}) ],
+				    0,
+				    function(x,ctx){return x+ctx['p']+ctx['q'];} ));
 	});
     });
     samples.forEach(function(n) {
@@ -36,13 +42,15 @@ suite('query',function() {
 	for (var i=0;i<n/2;i++) {
 	    store.add(["X",i,10]);
 	    store.add(["X",i,20]);
+	    store.add(["X",i,30]);
 	}
 	bench("3 headed, store"+n,function() {
-	    total=0;
-	    store.select(new chr.Select([["X",new chr.Variable('x'),new chr.Variable('p')],
-					 ["X",new chr.Variable('x'),new chr.Variable('q')] ],
-					function(ctx){return ctx['p']>ctx['q'];},
-					function(ctx){total+=ctx['p']+ctx['q'];} ));
+	    store.snap(new chr.Snap([new Match(["X",new chr.Variable('x'),new chr.Variable('p')]),
+				     new Match(["X",new chr.Variable('x'),new chr.Variable('q')]),
+				     new Match(["X",new chr.Variable('x'),new chr.Variable('r')]),
+				     new Guard(function(ctx){return ctx['p']>ctx['q'] && ctx['q']>ctx['r'];}) ],
+				    0,
+				    function(x,ctx){return x+ctx['p']+ctx['q']+ctx['r'];} ));
 	});
     });
 });
