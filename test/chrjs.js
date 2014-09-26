da87670cd0f4b22cfdf49e1c6e549e1bdc88bc0c
John Kozak <jk@thameslighter.net>
2014-09-26 21:33:32 +0100

    convert back to esprima for parsing

--- test/chrjs.js
+++ test/chrjs.js
@@ -1,29 +1,28 @@
-var sweet = require("sweet.js");
-var chrjs = require("../chrjs.js");
+var   chrjs = require("../chrjs.js");
 
-var assert = require("assert");
-var   temp = require('temp');
-var   util = require('../util.js');
-var   path = require('path');
-var     fs = require('fs');
+var  assert = require("assert");
+var    temp = require('temp');
+var    util = require('../util.js');
+var    path = require('path');
+var      fs = require('fs');
 
 temp.track();
 
-describe('chrjs parser',function() {
-    it("should parse map",function() {
-	var x = chrjs._private.compile("store { {a:[],c:22};};");
-	//console.log(util.format("*** macro expands to: %j",x));
-    });
-    it("should parse list",function() {
-	var x = chrjs._private.compile("store { ['X',19,{a:1}];};");
-	//console.log(util.format("*** macro expands to: %j",x));
-    });
-    it("should parse rule",function() {
-	var x = chrjs._private.compile("store { rule { +['A',a,...r1];(a==100);{B:a,...r2}} };");
-	//console.log(util.format("\n*** macro expands to: %j",x));
-    });
-    it("should parse named store",function() {
-	var x = chrjs._private.compile("store fred { rule { +['A',a,...r1];(a==100);b=10;{B:a,...r2}} };");
-	//console.log(util.format("\n*** macro expands to: %j",x));
-    });
+ describe('chrjs parser',function() {
+     it("should parse map",function() {
+ 	 var x = chrjs._private.parse("store { {a:[],c:22};};");
+ 	 //console.log(util.format("*** macro expands to: %j",x));
+     });
+     it("should parse list",function() {
+ 	 var x = chrjs._private.parse("store { ['X',19,{a:1}];};");
+ 	 //console.log(util.format("*** macro expands to: %j",x));
+     });
+     it("should parse rule",function() {
+ 	 var x = chrjs._private.parse("store { rule ( +['A',a,...r1],(a==100),{B:a,C,...r2}); };");
+ 	 //console.log(util.format("\n*** macro expands to: %j",x));
+     });
+     it("should parse named store",function() {
+ 	 var x = chrjs._private.parse("store fred { rule ( +['A',a,...r1],(a==100),b=10,{B:a,...r2}); };");
+ 	 //console.log(util.format("\n*** macro expands to: %j",x));
+     });
 });
---
+++ test/eschrjs.js
@@ -0,0 +1,371 @@
+var eschrjs = require("../eschrjs.js");
+
+var       _ = require('underscore');
+var  assert = require("assert");
+var    temp = require('temp');
+var    util = require('../util.js');
+var    path = require('path');
+var      fs = require('fs');
+var      vm = require('vm');
+
+temp.track();
+
+var chrjsTestFixture = {	// see esprima/test/test.js for format
+    'ChrJS expressions': {
+        'store{};': {
+            type: 'StoreStatement',
+	    id: null,
+            body: [],
+            range: [0, 7],
+            loc: {
+                start: { line: 1, column: 0 },
+                end: { line: 1, column: 7 }
+            }
+        },
+        'store{[]};': {
+            type: 'StoreStatement',
+	    id: null,
+            body: [
+		{
+		    type: 'ArrayExpression',
+		    elements: [],
+		    range: [6,8],
+		    loc: {
+			start: {line:1,column:6},
+			end: {line:1,column:8},
+		    } } ],
+            range: [0, 9],
+            loc: {
+                start: { line: 1, column: 0 },
+                end: { line: 1, column: 9 }
+            }
+        },
+        'store{{}};': {
+            type: 'StoreStatement',
+	    id: null,
+            body: [
+		{
+		    type: 'ObjectExpression',
+		    properties: [],
+		    range: [6,8],
+		    loc: {
+			start: {line:1,column:6},
+			end: {line:1,column:8},
+		    } } ],
+            range: [0, 9],
+            loc: {
+                start: { line: 1, column: 0 },
+                end: { line: 1, column: 9 }
+            }
+        }
+
+    }
+};
+
+describe("esprima-based parser",function() {
+    it("should pass original javascript tests",function() {
+	var total    = 0;
+	var failures = [];
+	vm.runInThisContext(fs.readFileSync(__dirname+'/../node_modules/esprima/test/test.js','utf-8'));
+	// this code largely taken from the esprima test runner
+	var adjustRegexLiteral = function(key,value) {
+	    if (key === 'value' && value instanceof RegExp) {
+		value = value.toString();
+	    }
+	    return value;
+	};
+	var NotMatchingError = function(expected,actual) {
+	    Error.call(this, 'Expected ');
+	    this.expected = expected;
+	    this.actual = actual;
+	}
+	NotMatchingError.prototype = new Error();
+	var errorToObject = function(e) {
+	    var msg = e.toString();
+
+	    // Opera 9.64 produces an non-standard string in toString().
+	    if (msg.substr(0, 6) !== 'Error:') {
+		if (typeof e.message === 'string') {
+		    msg = 'Error: ' + e.message;
+		}
+	    }
+	    
+	    return {
+		index: e.index,
+		lineNumber: e.lineNumber,
+		column: e.column,
+		message: msg
+	    };
+	};
+	var sortedObject = function(o) {
+	    if (o === null) {
+		return o;
+	    }
+	    if (o instanceof Array) {
+		return o.map(sortedObject);
+	    }
+	    if (typeof o !== 'object') {
+		return o;
+	    }
+	    if (o instanceof RegExp) {
+		return o;
+	    }
+	    var keys = Object.keys(o);
+	    var result = {
+		range: undefined,
+		loc: undefined
+	    };
+	    keys.forEach(function (key) {
+		if (o.hasOwnProperty(key)){
+		    result[key] = sortedObject(o[key]);
+		}
+	    });
+	    return result;
+	}
+	var hasAttachedComment = function(syntax) {
+	    var key;
+	    for (key in syntax) {
+		if (key === 'leadingComments' || key === 'trailingComments') {
+		    return true;
+		}
+		if (typeof syntax[key] === 'object' && syntax[key] !== null) {
+		    if (hasAttachedComment(syntax[key])) {
+			return true;
+		    }
+		}
+	    }
+	    return false;
+	}
+	var testParse = function(esprima,code,syntax) {
+	    var expected, tree, actual, options, StringObject, i, len, err;
+
+	    // alias, so that JSLint does not complain.
+	    StringObject = String;
+
+	    options = {
+		comment: (typeof syntax.comments !== 'undefined'),
+		range: true,
+		loc: true,
+		tokens: (typeof syntax.tokens !== 'undefined'),
+		raw: true,
+		tolerant: (typeof syntax.errors !== 'undefined'),
+		source: null
+	    };
+
+	    if (options.comment) {
+		options.attachComment = hasAttachedComment(syntax);
+	    }
+
+	    if (typeof syntax.tokens !== 'undefined') {
+		if (syntax.tokens.length > 0) {
+		    options.range = (typeof syntax.tokens[0].range !== 'undefined');
+		    options.loc = (typeof syntax.tokens[0].loc !== 'undefined');
+		}
+	    }
+
+	    if (typeof syntax.comments !== 'undefined') {
+		if (syntax.comments.length > 0) {
+		    options.range = (typeof syntax.comments[0].range !== 'undefined');
+		    options.loc = (typeof syntax.comments[0].loc !== 'undefined');
+		}
+	    }
+
+	    if (options.loc) {
+		options.source = syntax.loc.source;
+	    }
+
+	    syntax = sortedObject(syntax);
+	    expected = JSON.stringify(syntax, null, 4);
+	    try {
+		// Some variations of the options.
+		tree = esprima.parse(code, { tolerant: options.tolerant });
+		tree = esprima.parse(code, { tolerant: options.tolerant, range: true });
+		tree = esprima.parse(code, { tolerant: options.tolerant, loc: true });
+
+		tree = esprima.parse(code, options);
+		tree = (options.comment || options.tokens || options.tolerant) ? tree : tree.body[0];
+
+		if (options.tolerant) {
+		    for (i = 0, len = tree.errors.length; i < len; i += 1) {
+			tree.errors[i] = errorToObject(tree.errors[i]);
+		    }
+		}
+		tree = sortedObject(tree);
+		actual = JSON.stringify(tree, adjustRegexLiteral, 4);
+
+		// Only to ensure that there is no error when using string object.
+		esprima.parse(new StringObject(code), options);
+
+	    } catch (e) {
+		throw new NotMatchingError(expected, e.toString());
+	    }
+	    if (expected !== actual) {
+		throw new NotMatchingError(expected, actual);
+	    }
+
+	    function filter(key, value) {
+		if (key === 'value' && value instanceof RegExp) {
+		    value = value.toString();
+		}
+		return (key === 'loc' || key === 'range') ? undefined : value;
+	    }
+
+	    if (options.tolerant) {
+		return;
+	    }
+
+
+	    // Check again without any location info.
+	    options.range = false;
+	    options.loc = false;
+	    syntax = sortedObject(syntax);
+	    expected = JSON.stringify(syntax, filter, 4);
+	    try {
+		tree = esprima.parse(code, options);
+		tree = (options.comment || options.tokens) ? tree : tree.body[0];
+
+		if (options.tolerant) {
+		    for (i = 0, len = tree.errors.length; i < len; i += 1) {
+			tree.errors[i] = errorToObject(tree.errors[i]);
+		    }
+		}
+		tree = sortedObject(tree);
+		actual = JSON.stringify(tree, filter, 4);
+	    } catch (e) {
+		throw new NotMatchingError(expected, e.toString());
+	    }
+	    if (expected !== actual) {
+		throw new NotMatchingError(expected, actual);
+	    }
+	};
+
+	var testTokenize = function(esprima,code,tokens) {
+	    var options, expected, actual, tree;
+
+	    options = {
+		comment: true,
+		tolerant: true,
+		loc: true,
+		range: true
+	    };
+
+	    expected = JSON.stringify(tokens, null, 4);
+
+	    try {
+		tree = esprima.tokenize(code, options);
+		actual = JSON.stringify(tree, null, 4);
+	    } catch (e) {
+		throw new NotMatchingError(expected, e.toString());
+	    }
+	    if (expected !== actual) {
+		throw new NotMatchingError(expected, actual);
+	    }
+	};
+
+	var testError = function(esprima,code,exception) {
+	    var i, options, expected, actual, err, handleInvalidRegexFlag, tokenize;
+
+	    // Different parsing options should give the same error.
+	    options = [
+		{},
+		{ comment: true },
+		{ raw: true },
+		{ raw: true, comment: true }
+	    ];
+
+	    // If handleInvalidRegexFlag is true, an invalid flag in a regular expression
+	    // will throw an exception. In some old version V8, this is not the case
+	    // and hence handleInvalidRegexFlag is false.
+	    handleInvalidRegexFlag = false;
+	    try {
+		'test'.match(new RegExp('[a-z]', 'x'));
+	    } catch (e) {
+		handleInvalidRegexFlag = true;
+	    }
+
+	    exception.description = exception.message.replace(/Error: Line [0-9]+: /, '');
+
+	    if (exception.tokenize) {
+		tokenize = true;
+		exception.tokenize = undefined;
+	    }
+	    expected = JSON.stringify(exception);
+
+	    for (i = 0; i < options.length; i += 1) {
+
+		try {
+		    if (tokenize) {
+			esprima.tokenize(code, options[i])
+		    } else {
+			esprima.parse(code, options[i]);
+		    }
+		} catch (e) {
+		    err = errorToObject(e);
+		    err.description = e.description;
+		    actual = JSON.stringify(err);
+		}
+
+		if (expected !== actual) {
+
+		    // Compensate for old V8 which does not handle invalid flag.
+		    if (exception.message.indexOf('Invalid regular expression') > 0) {
+			if (typeof actual === 'undefined' && !handleInvalidRegexFlag) {
+			    return;
+			}
+		    }
+
+		    throw new NotMatchingError(expected, actual);
+		}
+
+	    }
+	};
+	var testAPI = function(esprima,code,result) {
+	    var expected, res, actual;
+	    expected = JSON.stringify(result.result, null, 4);
+	    try {
+		if (typeof result.property !== 'undefined') {
+		    res = esprima[result.property];
+		} else {
+		    res = esprima[result.call].apply(esprima, result.args);
+		}
+		actual = JSON.stringify(res, adjustRegexLiteral, 4);
+	    } catch (e) {
+		throw new NotMatchingError(expected, e.toString());
+	    }
+	    if (expected !== actual) {
+		throw new NotMatchingError(expected, actual);
+	    }
+	};
+	var runTest = function(esprima,code,result) {
+	    if (result.hasOwnProperty('lineNumber')) {
+		testError(esprima, code, result);
+	    } else if (result.hasOwnProperty('result')) {
+		testAPI(esprima, code, result);
+	    } else if (result instanceof Array) {
+		testTokenize(esprima, code, result);
+	    } else {
+		testParse(esprima, code, result);
+	    }
+	};
+	_.extend(testFixture,chrjsTestFixture); // add in tests for chrJS
+	//testFixture = chrjsTestFixture;           // !!! just testing mine for now !!!
+	Object.keys(testFixture).forEach(function(category) {
+            Object.keys(testFixture[category]).forEach(function(source) {
+		if (!(category==='API' && source==='Syntax')) { // this test will fail
+                    var expected = testFixture[category][source];
+                    total += 1;
+		    try {
+			runTest(eschrjs,source,expected);
+		    } catch (e) {
+			e.source = source;
+			// massage this into something I can actually read
+			console.log(util.format("\n%s.%s fails:\n wanted: %j\n got:    %j",
+						category,source,e.expected,e.actual));
+			failures.push(e);
+		    }
+		}
+            });
+	});
+	assert.strictEqual(failures.length,0);
+    });
+});
