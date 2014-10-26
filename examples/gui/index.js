e0bd04497d54d09c75924f9168ddfc5786f181c0
John Kozak <jk@thameslighter.net>
2014-10-26 11:27:49 +0000

    add setup.js config style; move node-webkit
    setup.js should be further abstracted into malaya.js
    
    Fix highlighting of big button

---
+++ examples/gui/index.js
@@ -0,0 +1,97 @@
+"use strict";
+
+// +++ sensible startup page, including:                        +++
+// +++ check if prevalence dir exists, offer create page if not +++
+
+var      _ = require('underscore');
+var     fs = require('fs');
+var   path = require('path');
+var   util = require('util');
+
+// +++ configure init and start buttons +++
+
+function brighten(elem,amount) {
+    var m = elem.css('background-color').match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+    var p = [0,0,0];
+    for (var i=0;i<3;i++) 
+        p[i] = Math.round(parseInt(m[i+1])*amount);
+    elem.css('background-color',"rgb("+p[0]+","+p[1]+","+p[2]+')');
+}
+
+$(document).ready(function() {
+    var  state = 'stopped';	// 'starting','started','stopping'
+    var   opts = {};
+    var cFacts = 0;
+    var cConns = 0;
+    var    dir = fs.realpathSync('../idb');	// +++ should be selected in the GUI
+    var  build = require(path.join(dir,'setup.js'));
+    var server;
+    opts.fe3Port       = 5110
+    opts.port          = 3000;
+    opts.prevalenceDir = path.join(dir,'.prevalence');
+    opts.businessLogic = path.join(dir,'bl.chrjs');
+    opts.tag           = 'idb';
+    opts.init          = false;
+    opts.audit         = true;
+    opts.webDir        = fs.realpathSync('.');
+    opts.auto_output   = true;
+    // set parameter table
+    $('#paramTag') .text(opts.tag);
+    $('#paramPort').text(opts.port);
+    $('#paramFE3P').text(opts.fe3Port);
+    $('#paramBL')  .text(opts.businessLogic);
+    $('#paramPrvD').text(opts.prevalenceDir);
+    // +++ get from above +++
+    //if (!(fs.existsSync(opts.prevalenceDir)))
+    //	opts.init = true;
+    $('#bigButton').hover(function() {brighten($(this),1.2)},
+			  function() {brighten($(this),0.83333)} );
+    $('#bigButton').click(function() {
+	$('#bigButton').css('backgroundColor','rgb(198,198,198)');
+	$('#bigButton').text('');
+	setTimeout(function() {
+	    switch (state) {
+	    case 'started':
+		state = 'stopping';
+		server.close();
+		break;
+	    case 'stopped':
+		state  = 'starting';
+		server = build.build(opts);
+		process.on('SIGHUP',function() {server.save();});
+		server.fe3.on('listening',function() {
+		    cFacts = server.size;
+		    $('#paramNF').text(cFacts);
+		    $('#paramNC').text(cConns);
+		    server.on('makeConnection',function() {$('#paramNC').text(++cConns);});
+		    server.on('loseConnection',function() {$('#paramNC').text(--cConns);});
+		    server.ready();
+		});
+		server.on('loaded',function(hash) {
+		    // !!! this is now called too late to do anything !!!
+		    $('#narrHash').text("opening hash");
+		    $('#textHash').text(hash);
+		});
+		server.on('ready',function() {
+		    $('#bigButton').css('background','rgb(254,0,0)');
+		    $('#bigButton').text('STOP');
+		    state = 'started';
+		});
+		server.on('closed',function(hash) {
+		    $('#narrHash').text("closing hash");
+		    $('#textHash').text(hash);
+		    $('#bigButton').css('background','rgb(0,254,0)');
+		    $('#bigButton').text('START');
+		    state = 'stopped';
+		});
+		server.on('fire',function(obj,fact,adds,dels) {
+		    cFacts += adds.length;
+		    cFacts -= dels.length;
+		    $('#paramNF').text(cFacts);
+		});
+		server.run();
+		break;
+	    }
+	},0);
+    });
+});
---
+++ examples/idb/setup.js
@@ -0,0 +1,112 @@
+"use strict";
+
+var      _ = require('underscore');
+var   util = require('../../util.js');
+var   path = require('path');
+var malaya = require('../../malaya.js')
+var    fe3 = require('./fe3.js');
+
+exports.build = function(opts) {
+    opts = _.extend({
+	prevalenceDir: path.join(__dirname,'.prevalence'),
+	webDir:        'www',
+	audit:         true,
+	logging:       true,
+	init:          false,
+	tag:           'idb',
+	businessLogic: path.join(__dirname,'bl.chrjs'),
+	auto_output:   true},
+		    opts);
+    
+    var  server = malaya.createServer(opts);
+    var  fe3srv = fe3.createServer({malaya:server});
+    var fe3port = opts.fe3port || 5110;
+    var  wsport = opts.wsport  || 3000;
+    
+    fe3srv.on('connect',function(mc) {
+	server.addConnection(mc);
+    });
+    fe3srv.on('listening',function() {
+	util.debug('fe3  listening on *:%s',fe3port);
+	server.ready();
+    });
+    var timer = setInterval(function() {
+	server.command(['tick',{date:new Date()}],{port:'server:'});
+    },1000);
+
+    server.on('loaded',function(hash) {
+	util.debug("opening hash is: %s",hash);
+    });
+    server.on('closed',function(hash) {
+	util.debug("closing hash is: %s",hash);
+    });
+    server.on('closed',function() {
+	if (timer)
+	    clearInterval(timer);
+	fe3srv.close();
+	fe3srv = null;
+    });
+
+    server.start();
+    
+    server.on('makeConnection',function(mc) {
+	util.debug("hello, %j",mc);
+    });
+    server.on('loseConnection',function(mc) {
+	util.debug("farewell, %j",mc);
+    });
+    
+    server.run = function() {
+	var listen = function() {
+	    server.command(['restart',{}],{port:'server:'});
+	    server.listen(wsport,function() {
+		fe3srv.listen(fe3port);
+	    });
+	};
+	if (!!opts.init)
+	    init(server,opts.init,listen);
+	else
+	    listen();
+    };
+
+    server.fe3 = fe3srv;
+    
+    return server;
+};
+
+function init(server,init,listen) {
+    // +++ do this via the `transform` mechanism that doesn't exist yet [b8cb5936e7df0244] +++
+    var IDB = {add:function(js) {server.command(js,{port:'init'});}};
+    if (init.match(/\.json$/)) {
+	var json = JSON.parse(fs.readFileSync(init));
+	for (var i in json) {
+	    IDB.add(json[i]);
+	}
+	listen();
+    } else if (init.match(/\.py$/)) {
+	var  exec = require('child_process').exec;
+	var child = exec("python import_init_db.py "+init,{
+	    maxBuffer: 10*1024*1024
+	}, function(err,stdout,stderr) {
+	    var json = JSON.parse(stdout);
+	    for (var i in json)
+		IDB.add(json[i]);
+	});
+	child.on('exit',function(code,signal) {
+	    if (code!==0)
+		console.log("failed: %j",code);
+	});
+	child.on('close',function() {
+	    listen();
+	});
+    } else if (init.match(/[a-zA-Z0-9]+@[a-zA-Z0-9-]+\/[a-z0-9]+:.*/)) {
+	throw new Error("NYI - ODBC loading");
+    } else {
+	console.error("can't init from: %j",init);
+	process.exit(100);
+    }
+}
+
+
+
+
