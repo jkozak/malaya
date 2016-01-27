2d189740b4fb6c09646e2cbf0cadcd3c5db3187d
John Kozak <jk@thameslighter.net>
2016-01-27 18:32:19 +0000

    add 'web' example

---
+++ examples/web/web.js
@@ -0,0 +1,46 @@
+"use strict";
+
+const       path = require('path');
+const       util = require('malaya/util.js');
+const     Engine = require('malaya/engine.js').Engine;
+const    express = require('malaya/engine.js').express;
+const bodyParser = require('body-parser');
+
+function WebEngine(options) {
+    const eng = this;
+    options.bundles = {
+        "/bundle.js":[path.join(process.cwd(),'www/index.js')]
+    };
+    options.logging = true;
+    Engine.call(eng,options);
+    return eng;
+}
+util.inherits(WebEngine,Engine);
+
+WebEngine.prototype.createExpressApp = function() {
+    const eng = this;
+    const app = Engine.prototype.createExpressApp.call(eng);
+
+    app.use('/static/',express.static('www'));
+    app.use(bodyParser.urlencoded({extended:true}));
+    app.all('/REST/*',function(req,res){
+        console.log("   req: %j %j %j",req.method,req.url,req.params);
+        if (req.method==='POST')
+            console.log("    body: %j",req.body);
+        eng.update(['http',{method:req.method,
+                            url:   req.url,
+                            params:req.params,
+                            body:  req.body}]);
+        res.send(200);
+    });
+    app.get('/',function(res,req){
+        res.redirect('/static/index.html');
+    });
+
+    // +++ setup output handler for engine +++
+
+    return app;
+};
+
+
+exports.Engine = WebEngine;
