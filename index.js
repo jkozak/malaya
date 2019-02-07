f64fbe8f95e2e7accbe9952c15812906ecc21e69
John Kozak <jk@thameslighter.net>
2019-02-07 19:22:52 +0000

    export testutil

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ exports.cmdline  = require('./cmdline.js');
 exports.compiler = require('./compiler.js'); // also adds .chrjs extension to `require`
 exports.engine   = require('./engine.js');
 exports.util     = require('./util.js');
+exports.testutil = require('./testutil.js');
 
 exports.load     = require('./compiler.js').load;
