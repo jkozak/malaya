f6f6c5cefd50b1dcd64787abd2a7d779786197c5
John Kozak <jk@thameslighter.net>
2019-02-25 15:09:12 +0000

    PEG parser [WIP]

---
+++ parser2.js
@@ -0,0 +1,19 @@
+"use strict";
+
+// Stub to compile and load parser.pegjs
+// will be pre-done by a build procedure eventually
+
+const    fs = require('fs');
+const   peg = require('pegjs');
+const  path = require('path');
+const  util = require('./util.js');
+
+module.exports = peg.generate(fs.readFileSync(path.join(__dirname,'parser2.pegjs'),'utf8'),
+                              util.env==='test' ?
+                              {
+                                  trace: false,
+                                  allowedStartRules: [
+                                      'Start',
+                                      'JsonMatch','RuleItem','FunctionExpression'
+                                  ]
+                              } : {} );
