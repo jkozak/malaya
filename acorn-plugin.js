81b45d2155f707adb96f4b6f8dfde6ac414b9ad1
John Kozak <jk@thameslighter.net>
2022-01-05 13:16:56 +0000

    acorn-plugin [WIP]

--- acorn-plugin.js
+++ acorn-plugin.js
@@ -8,86 +8,165 @@ module.exports = function malaya(Parser) {
     return class extends Parser {
         constructor(opts,pos,extensions) {
             super(opts,pos,extensions);
-            this.inStore = false;
+            this.inStore = opts.inStore || false;
+            this.inRule  = opts.inRule  || false;
         }
         readToken(code) {
             super.readToken(code);
-            console.log(`*** read `,this.type.label);
+            //console.log(`*** read `,this.type.label);
+        }
+        parseArrayOrWhereExpression(node,allowTrailingComma,allowEmpty,rDE) {
+            node.elements = []
+            if (allowTrailingComma || allowEmpty)
+                throw new Error(`NYI`);
+            if (this.type!==tt.bracketL)
+                throw new Error(`SNO`);
+            this.next();
+            for (let first=true;;first=false) {
+                if (this.eat(tt.bracketR))
+                    return this.finishNode(node,'ArrayExpression');
+                if (this.inStore && !this.inRule &&
+                    this.type===tt.name && this.value==='where') {
+                    this.next();
+                    node.rules = this.parseMalayaRuleBody(this.startNode());
+                    return this.finishNode(node,'WhereExpression');
+                }
+                if (!first)
+                    this.expect(tt.comma);
+                let elt;
+                if (this.type===tt.ellipsis)
+                    elt = this.parseSpread(rDE);
+                 else
+                    elt = this.parseMaybeAssign(false,rDE)
+                node.elements.push(elt)
+            }
         }
         parseExprAtom(rDE,fI) {
-            const r =  super.parseExprAtom(rDE,fI);
-            console.log(`pEA: `,r);
-            if (r.type==='Identifier' && r.name==='store') {
-                const node = this.startNode();
-                return this.parseStoreBody(node,0);
+            if (this.inStore && this.type===tt.bracketL) {
+                return this.parseArrayOrWhereExpression(this.startNode(),false,false,rDE);
+            } else {
+                const node = super.parseExprAtom(rDE,fI);
+                if (node.type==='Identifier' && node.name==='store')
+                    return this.parseStoreBody(this.startNode(),0);
+                return node;
             }
-            return r;
         }
         parseStoreBody(node,statement) {
             let inside = true;
-            node.items = [];
-            node.rules = [];
-            console.log(`*** store body`);
+            node.items      = [];
+            node.rules      = [];
+            node.queries    = [];
+            node.invariants = [];
             this.expect(tt.braceL);
+            this.inStore = true;
             while (inside) {
                 switch (this.type) {
                 case tt.braceR:
                     inside = false;
                     break;
                 case tt.name:
-                    if (this.value==='rule') {
+                    if (this.value==='rule')
                         node.rules.push(this.parseMalayaRule(this.startNode()));
-                        break;
-                    } else
+                    else if (this.value==='query')
+                        node.queries.push(this.parseMalayaQuery(this.startNode()));
+                    else if (this.value==='invariant')
+                        node.invariants.push(this.parseMalayaInvariant(this.startNode()));
+                    else
                         throw new Error(`??? ${JSON.stringify(this.type)}`);
-                case tt.bracketL:
-                case tt.braceL:
-                    node.items.push(this.parseMalayaItem(this.startNode(),true));
-                    this.expect(';');
+                    this.expect(tt.semi);
                     break;
                 default:
-                    throw new Error(`??? ${JSON.stringify(this.type)} ${JSON.stringify(this.value)}`);
+                    node.items.push(this.parseExpression(false));
+                    this.expect(tt.semi);
                 }
             }
+            this.inStore = false;
             this.expect(tt.braceR);
-            return this.finishNode(node,"StoreExpression");
+            return this.finishNode(node,'StoreExpression');
+        }
+        checkPropClash(prop,propHash,rDE) {
+            if (this.inStore)
+                return;
+            return super.checkPropClash(prop,propHash,rDE);
+        }
+        parseMalayaEllipsis(node) {
+            this.next();
+            node.bind = this.type===tt.name ? this.parseIdent() : null;
+            return this.finishNode(node,'MalayaEllipsis');
+        }
+        parseProperty(iP,rDE) {
+            if (this.inStore && this.type===tt.ellipsis)
+                return this.parseMalayaEllipsis(this.startNode());
+            return super.parseProperty(iP,rDE);
+        }
+        parseSpread(rDE) {
+            if (this.inStore)
+                return this.parseMalayaEllipsis(this.startNode());
+            else
+                return super.parseSpread(rDE);
+        }
+        importMalayaItem(node) {
+            const n = new acorn.Node(this,node.start,node.loc);
+            n.type = 'MalayaItem';
+            n.rank = null;
+            switch (node.type) {
+            case 'UnaryExpression':
+                n.op   = node.operator;
+                n.expr = node.argument;
+                break;
+            case 'AssignmentExpression':
+                n.op   = '=';
+                n.expr = node;
+                break;
+            case 'ArrayExpression':
+            case 'ObjectExpression':
+                n.op   = 'M';
+                n.expr = node;
+                break;
+            case 'BinaryExpression':
+            case 'CallExpression':
+                n.op   = '?';
+                n.expr = node;
+                break;
+            default:
+                console.log(node);
+                throw new Error(`NYI: ${node.type}`);
+            }
+            return n;
         }
         parseMalayaItem(node) {
-            if (this.type===tt.braceL)
-                this.parseObj(true);
-            else if (this.type===tt.bracketL)
-                this.parseExprList(tt.bracketR,false,true);
+            node.item = this.importMalayaItem(this.parseExpression(false));
+            return this.finishNode(node,'MalayaItem');
+        }
+        parseMalayaRuleBody() {
+            const expr = this.parseExpression(false);
+            if (expr.type==='SequenceExpression')
+                return expr.expressions.map(this.importMalayaItem.bind(this));
             else
-                throw new Error(`???`,node);
-            return this.finishNode(node,"MalayaItem");
+                return [this.importMalayaItem(expr)];
         }
         parseMalayaRule(node) {
-            console.log(`*** parseMalayaRule`);
             this.next();
-            node.items = [];
             this.expect(tt.parenL);
-            for (;;) {
-                if (this.type===tt.plusMin) {
-                    const op = this.value;
-                    this.next();
-                    const item = this.parseMalayaItem();
-                    item.op = op;
-                    node.items.push(item);
-                } else if (this.type===tt.parenR) {
-                    this.next();
-                    break;
-                } else if (this.type===tt.braceL || this.type===tt.bracketL) {
-                    node.items.push(this.parseMalayaItem(this.startNode()));
-                    this.expect(';');
-                } else {
-                    // +++ logical expr or match +++
-                }
-                this.next();
-                if (this.type!==tt.parenR)
-                    this.expect(tt.comma);
-            }
-            this.expect(tt.semi);
-            return this.finishNode(node,"MalayaRule");
+            this.inRule = true;
+            node.items  = this.parseMalayaRuleBody();
+            this.inRule = false;
+            this.expect(tt.parenR);
+            return this.finishNode(node,'MalayaRule');
+        }
+        parseMalayaQuery(node) {
+            this.next();
+            node.id = this.type===tt.name ? this.parseIdent(false,false) : null;
+            this.expect(tt.parenL);
+            node.args = this.parseExprList(tt.parenR);
+            node.body = this.parseExpression(this.startNode());
+            return this.finishNode(node,'MalayaQuery');
+        }
+        parseMalayaInvariant(node) {
+            this.next();
+            node.id   = this.type===tt.name ? this.parseIdent(false,false) : null;
+            node.expr = this.parseExpression(false);
+            return this.finishNode(node,'MalayaInvariant');
         }
     };
 };
--- test/acorn-plugin.js
+++ test/acorn-plugin.js
@@ -2,8 +2,11 @@
 
 const acornPlugin = require('../acorn-plugin.js');
 
+const     fs = require('fs');
 const  acorn = require('acorn');
 const assert = require('assert').strict;
+
+acorn.defaultOptions.inStore = false;
 
 const parser = acorn.Parser.extend(
     Parser=>class extends acornPlugin(Parser) {
@@ -17,12 +20,131 @@ const parser = acorn.Parser.extend(
         }
     });
 
-const parse = (nt,x)=>parser._parseFrom(nt,x,{ecmaVersion:2022});
+const parse = (nt,x,opts)=>parser._parseFrom(nt,x,{ecmaVersion:2022,...opts});
 
 describe("acorn parser plugin XXX",function(){
-    it("handles empty store",function(){
-        const sb = parse("StoreBody","{}")
-        assert.equal(sb.items.length,0);
-        assert.equal(sb.rules.length,0);
+    describe("item",function(){
+        it("list",function(){
+            const sb = parse('MalayaItem',"['a',{}]",{inStore:true});
+            console.log(sb);
+        });
+        it("list with ellipsis",function(){
+            const sb = parse('MalayaItem',"['a',...]",{inStore:true});
+            console.log(sb);
+        });
+        it("list with named ellipsis",function(){
+            const sb = parse('MalayaItem',"['a',...x]",{inStore:true});
+            console.log(sb);
+        });
+        it("list with penultimate ellipsis",function(){
+            const sb = parse('MalayaItem',"['a',...,x]",{inStore:true});
+            console.log(sb);
+        });
+        it("object",function(){ //
+            const sb = parse('MalayaItem',"{a:2}",{inStore:true});
+            console.log(sb);
+        });
+        it("object with ellipsis",function(){ //
+            const sb = parse('MalayaItem',"{a:2,...}",{inStore:true});
+            console.log(sb);
+        });
+        it("object with named ellipsis",function(){ //
+            const sb = parse('MalayaItem',"{...x,a:2}",{inStore:true});
+            console.log(sb);
+        });
+        it("object with implicit binding",function(){ //
+            const sb = parse('MalayaItem',"{x,a:2}",{inStore:true});
+            console.log(sb);
+        });
+        it("conditional",function(){
+            const sb = parse('MalayaItem',"a>18",{inStore:true});
+            console.log(sb);
+        });
+        it("tricky conditional",function(){
+            const sb = parse('MalayaItem',"['x','y'].includes(p)",{inStore:true});
+            console.log(sb);
+        });
+    });
+    describe("rule body",function(){
+        it("bans empty",function(){
+            assert.throws(()=>{
+                parse('MalayaRuleBody',"",{inStore:true});
+            });
+        });
+        it("only deletes",function(){
+            const sb = parse('MalayaRuleBody',"-['a',{}]",{inStore:true});
+            assert(Array.isArray(sb));
+            assert.equal(sb.length,1);
+            assert.equal(sb[0].type,'MalayaItem');
+            assert.equal(sb[0].op,  '-');
+        });
+        it("deletes and adds",function(){
+            const sb = parse('MalayaRuleBody',"-['a',{p}],+['b',{p}]",{inStore:true});
+            assert(Array.isArray(sb));
+            assert.equal(sb.length,2);
+            sb.forEach(s=>assert(s.type==='MalayaItem'));
+            assert.equal(sb[0].op,'-');
+            assert.equal(sb[1].op,'+');
+        });
+        it("match and bind",function(){
+            const sb = parse('MalayaRuleBody',"-['a',{p}],pp=p.p",{inStore:true});
+            assert(Array.isArray(sb));
+            assert.equal(sb.length,2);
+            sb.forEach(s=>assert(s.type==='MalayaItem'));
+            assert.equal(sb[0].op,       '-');
+            assert.equal(sb[1].op,       '=');
+            assert.equal(sb[1].expr.type,'AssignmentExpression');
+        });
+    });
+    describe("rule",function(){
+        it("bans empty",function(){
+            assert.throws(()=>{
+                parse('MalayaRule',"rule ()",{inStore:true});
+            });
+        });
+        it("delete, add, bind, match",function(){
+            const sb = parse('MalayaRule',"rule (-['a',{p}],+['b',{p}],pp=p.p,pp>18)",{inStore:true});
+            assert.equal(sb.type,'MalayaRule');
+            assert(Array.isArray(sb.items));
+            const ss = sb.items;
+            assert.equal(ss.length,4);
+            ss.forEach(s=>assert(s.type==='MalayaItem'));
+            assert.equal(ss[0].op,'-');
+            assert.equal(ss[1].op,'+');
+            assert.equal(ss[2].op,'=');
+            assert.equal(ss[3].op,'?');
+        });
+    });
+    describe("where expression XXX",function(){
+        it("simple",function(){
+            const sb = parse('Expression',"[x where ['what',{x,...}]]",{inStore:true});
+            console.log(sb);
+        });
+    });
+    describe("query",function(){
+    });
+    describe("invariant",function(){
+    });
+    describe("store",function(){
+        it("empty",function(){
+            const sb = parse('StoreBody',"{}")
+            console.log(sb);
+        });
+        it("lone fact",function(){
+            const sb = parse('StoreBody',"{['a',{p:17}];}");
+            console.log(sb);
+        });
+        it("pre-existing 1",function(){
+            const sb = parse('TopLevel',fs.readFileSync('test/bl/count.malaya'));
+            console.log(sb);
+        });
+        it("pre-existing 2",function(){
+            const sb = parse('TopLevel',fs.readFileSync('test/bl/middleware.malaya'));
+            console.log(sb);
+        });
+        xit("pre-existing 3",function(){
+            const sb = parse('TopLevel',fs.readFileSync('../flatland/index.malaya'));
+            console.log(sb);
+        });
     });
 });
