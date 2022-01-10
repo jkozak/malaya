fe7a303bb4aa2624a258b1e143e675d6ed1f7097
John Kozak <jk@thameslighter.net>
2022-01-10 18:34:44 +0000

    tidy up

--- shell.nix
+++ shell.nix
@@ -1,6 +1,6 @@
 with import <nixpkgs> {};
 stdenv.mkDerivation {
   name = "malaya-build-env";
-  buildInputs = [ systemd nodejs ];
+  buildInputs = [ systemd nodejs jq ];
   shellHook = ''export PATH=./node_modules/.bin:$PATH'';
 }
