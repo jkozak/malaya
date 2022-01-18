14fb763071889af60da41497b637832fc6a8d92a
John Kozak <jk@thameslighter.net>
2022-01-18 10:54:27 +0000

    switch to node 16 for dev, support >=14

--- shell.nix
+++ shell.nix
@@ -1,6 +1,6 @@
 with import <nixpkgs> {};
 stdenv.mkDerivation {
   name = "malaya-build-env";
-  buildInputs = [ systemd nodejs jq ];
+  buildInputs = [ systemd nodejs-16_x jq ];
   shellHook = ''export PATH=./node_modules/.bin:$PATH'';
 }
