4d31f7936fe971c4d267a0652fa6868527c6a463
John Kozak <jk@thameslighter.net>
2022-07-20 16:49:53 +0100

    debug npm install [TEST]

--- for-sub-npm-modules.js
+++ for-sub-npm-modules.js
@@ -7,9 +7,11 @@ const   fs = require('fs');
 const path = require('path');
 
 process.argv.slice(3).forEach(d=>{
+    console.log(`*** start: ${d}`);
     fs.readdirSync(d)
         .map(f=>path.join(d,f))
         .filter(f=>fs.statSync(f).isDirectory())
         .filter(f=>fs.existsSync(path.join(f,'package.json')))
+        .map(f=>(console.log(`*** path: ${f}`),f))
         .forEach(f=>cp.execSync(process.argv[2],{cwd:f,stdio:'inherit'}));
 });
