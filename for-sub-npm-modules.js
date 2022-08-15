26477dc7915a43987a03a11f852447a45a4e067c
John Kozak <jk@thameslighter.net>
2022-08-15 09:04:48 +0100

    remove troublesome internal plugin submodules
    Worked fine for ages but not now.  Think it's npm 7+
    
    Make fetch and systemd external plugins one day.

--- for-sub-npm-modules.js
+++ for-sub-npm-modules.js
@@ -7,10 +7,11 @@ const   fs = require('fs');
 const path = require('path');
 
 process.argv.slice(3).forEach(d=>{
+    console.log(`*** for-sub-modules: enter directory ${d}`);
     fs.readdirSync(d)
         .filter(f=>!f.startsWith('.'))
         .map(f=>path.join(d,f))
         .filter(f=>fs.statSync(f).isDirectory())
         .filter(f=>fs.existsSync(path.join(f,'package.json')))
-        .forEach(f=>cp.execSync(process.argv[2],{cwd:f,stdio:'inherit'}));
+        .forEach(f=>cp.execSync(`pwd;${process.argv[2]}`,{cwd:f,stdio:[null,'inherit','inherit']}));
 });
